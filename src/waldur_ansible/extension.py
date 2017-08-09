eb18c332e6c2764dccc6363d640b27ef87152213
Victor Mireyev <victor@opennodecloud.com>
2017-08-09 22:45:09 +0300

    Remove deprecated parameter from default plugin configuration [WAL-1099]

--- src/waldur_ansible/extension.py
+++ src/waldur_ansible/extension.py
@@ -6,7 +6,6 @@ class AnsibleExtension(NodeConductorExtension):
     class Settings:
         WALDUR_ANSIBLE = {
             'PLAYBOOKS_DIR_NAME': 'ansible_playbooks',
-            'PRESERVE_PLAYBOOK_ARCHIVE_AFTER_DELETION': False,
             'PLAYBOOK_EXECUTION_COMMAND': 'ansible-playbook',
             'PLAYBOOK_ARGUMENTS': ['--verbose'],
             'ANSIBLE_LIBRARY': '/usr/share/ansible-waldur/',
