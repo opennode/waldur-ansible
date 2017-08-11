2edd27c4057df41c7276a8c272fd3935a3e6407d
Victor Mireyev <victor@opennodecloud.com>
2017-08-11 17:01:13 +0300

    Add counter for applications [WAL-1076]

--- src/waldur_ansible/views.py
+++ src/waldur_ansible/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from nodeconductor.core import mixins as core_mixins
 from nodeconductor.core import validators as core_validators
 from nodeconductor.core import views as core_views
+from nodeconductor.structure import views as structure_views
 from nodeconductor.structure.filters import GenericRoleFilter
 from nodeconductor.structure.metadata import ActionsMetadata
 from nodeconductor.structure.permissions import is_staff, is_administrator
@@ -27,3 +28,9 @@ class JobViewSet(core_mixins.CreateExecutorMixin, core_views.ActionsViewSet):
     metadata_class = ActionsMetadata
     destroy_validators = [core_validators.StateValidator(models.Job.States.OK, models.Job.States.ERRED)]
     create_executor = executors.RunJobExecutor
+
+
+def get_project_jobs_count(project):
+    return models.Job.objects.filter(service_project_link__project=project).count()
+
+structure_views.ProjectCountersView.register_counter('ansible', get_project_jobs_count)
