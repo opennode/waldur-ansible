74fc95b3fa5cf589b7eb7e9d9a6635c6d5824be9
Victor Mireyev <victor@opennodecloud.com>
2017-10-18 20:31:30 +0300

    Fix unit test.

--- src/waldur_ansible/tests/test_job.py
+++ src/waldur_ansible/tests/test_job.py
@@ -132,7 +132,7 @@ class JobDeleteTest(JobBaseTest):
 
         self.client.force_authenticate(self.fixture.staff)
         response = self.client.delete(factories.JobFactory.get_url(self.job))
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
 
 
 class CountersTest(JobBaseTest):
