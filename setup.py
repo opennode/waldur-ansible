4fff54aa4ec41f1b0823d244d8e5c28e335276ad
Jenkins <jenkins@opennodecloud.com>
2018-03-06 13:20:09 +0000

    Preparing new release: 0.4.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setup(
     name='waldur-ansible',
-    version='0.3.3',
+    version='0.4.0',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://waldur.com',
