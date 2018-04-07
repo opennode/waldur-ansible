fbd9d4eb8203c4f3b5f773ce12d46f6a87c843a4
Stanislav Nazmutdinov <nazmutdinov@gmail.com>
2018-04-07 21:25:55 +0300

    [WAL-1449] Fixed an issue when logic would not delete jupyter_hub_management

--- src/waldur_ansible/jupyter_hub_management/backend/error_handlers.py
+++ src/waldur_ansible/jupyter_hub_management/backend/error_handlers.py
@@ -5,7 +5,7 @@ from waldur_core.core import models as core_models
 class DeleteRequestErrorHandler(object):
     def handle_error(self, request, lines_post_processor):
         if not request.jupyter_hub_management.instance or self.exists_failed_initialization_request(request.jupyter_hub_management):
-            request.python_management.delete()
+            request.jupyter_hub_management.delete()
 
     def exists_failed_initialization_request(self, jupyter_hub_management):
         return models.JupyterHubManagementSyncConfigurationRequest.objects.filter(jupyter_hub_management=jupyter_hub_management).latest('id').state \
