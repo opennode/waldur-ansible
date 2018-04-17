12335a21724b70850ca8f500dcc58ed06b5ecc4d
Stanislav Nazmutdinov <nazmutdinov@gmail.com>
2018-04-17 23:54:59 +0300

    [WAL-1449] Now test properly override settings without breaking isolation

--- src/waldur_ansible/jupyter_hub_management/tests/integration/test_jupyter_hub_management_backend_deployment.py
+++ src/waldur_ansible/jupyter_hub_management/tests/integration/test_jupyter_hub_management_backend_deployment.py
@@ -4,7 +4,6 @@ import urllib3
 
 from mock import patch
 from django.conf import settings
-from django.test import override_settings
 from django.test import TestCase
 
 from waldur_ansible.common.tests.integration.ubuntu1604_container import Ubuntu1604Container, CONTAINER_SSH_PORT_ON_HOST
@@ -24,36 +23,34 @@ class PythonManagementIntegrationTest(TestCase):
         self.module_path = "waldur_ansible.python_management.backend.python_management_backend."
 
     @pytest.mark.integration
-    @override_settings()
     def test_jupyter_hub_initialization(self):
-        self.prepare_environment()
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        waldur_ansible_common_settings = settings.WALDUR_ANSIBLE_COMMON.copy()
+        waldur_ansible_common_settings["REMOTE_VM_SSH_PORT"] = CONTAINER_SSH_PORT_ON_HOST
+        waldur_ansible_common_settings["PRIVATE_KEY_PATH"] = Ubuntu1604Container.get_private_key_path()
 
-        ubuntu_container = Ubuntu1604Container()
-        container_https_port = "443/tcp"
-        ubuntu_container.bind_port(container_https_port, "4444")
+        with self.settings(WALDUR_ANSIBLE_COMMON=waldur_ansible_common_settings):
+            ubuntu_container = Ubuntu1604Container()
+            container_https_port = "443/tcp"
+            ubuntu_container.bind_port(container_https_port, "4444")
 
-        with ubuntu_container:
-            python_management = self.fixture.jupyter_hub_admin_user.jupyter_hub_management.python_management
-            self.adapt_instance_external_ip(python_management, ubuntu_container)
+            with ubuntu_container:
+                python_management = self.fixture.jupyter_hub_admin_user.jupyter_hub_management.python_management
+                self.adapt_instance_external_ip(python_management, ubuntu_container)
 
-            with patch(self.module_path + "locking_service.PythonManagementBackendLockingService") as locking_service:
-                locking_service.is_processing_allowed.return_value = True
+                with patch(self.module_path + "locking_service.PythonManagementBackendLockingService") as locking_service:
+                    locking_service.is_processing_allowed.return_value = True
 
-                python_management_init_request = python_management_factories.PythonManagementInitializeRequestFactory(python_management=python_management, output="")
-                python_management_init_request.get_backend().process_python_management_request(python_management_init_request)
+                    python_management_init_request = python_management_factories.PythonManagementInitializeRequestFactory(python_management=python_management, output="")
+                    python_management_init_request.get_backend().process_python_management_request(python_management_init_request)
 
-                jup_init_request = jupyter_hub_factories.JupyterHubManagementSyncConfigurationRequestFactory(
-                    jupyter_hub_management=self.fixture.jupyter_hub_management_linux_pam, output="")
-                jup_init_request.get_backend().process_jupyter_hub_management_request(jup_init_request)
+                    jup_init_request = jupyter_hub_factories.JupyterHubManagementSyncConfigurationRequestFactory(
+                        jupyter_hub_management=self.fixture.jupyter_hub_management_linux_pam, output="")
+                    jup_init_request.get_backend().process_jupyter_hub_management_request(jup_init_request)
 
-            jupyter_hub_request = requests.get("https://%s:%s" % (ubuntu_container.get_container_host_ip(), ubuntu_container.ports[container_https_port]), verify=False)
-            self.assertEquals(jupyter_hub_request.status_code, 200)
+                jupyter_hub_request = requests.get("https://%s:%s" % (ubuntu_container.get_container_host_ip(), ubuntu_container.ports[container_https_port]), verify=False)
+                self.assertEquals(jupyter_hub_request.status_code, 200)
 
     def adapt_instance_external_ip(self, python_management, ubuntu_container):
         container_host_ip = ubuntu_container.get_container_host_ip()
         openstack_tenant_models.FloatingIP.objects.filter(id=python_management.instance.floating_ips[0].id).update(address=container_host_ip, name=container_host_ip)
-
-    def prepare_environment(self):
-        settings.WALDUR_ANSIBLE_COMMON["REMOTE_VM_SSH_PORT"] = CONTAINER_SSH_PORT_ON_HOST
-        settings.WALDUR_ANSIBLE_COMMON["PRIVATE_KEY_PATH"] = Ubuntu1604Container.get_private_key_path()
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
